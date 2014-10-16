d4860e153ed56746a493ad80e6e4b998191957bb
Enzo Reyes <enzo.adrianreyes@gmail.com>
2014-10-16 11:45:33 +1100

    Changed the path in app.'js to match the current layout.

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./web/routes/index');
+var users = require('./web/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'web/views'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'web/public')));
 
 app.use('/', routes);
 app.use('/users', users);
